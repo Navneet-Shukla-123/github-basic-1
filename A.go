dfac03838204ac34c1eb4ac73410082bbccf681c
Navneet-Shukla-123 <Navneet.shukla@pantelwar.com>
2024-05-21 06:33:58 +0530

    change

--- A.go
+++ A.go
@@ -7,6 +7,3 @@ func A(){
 	fmt.Println("This is the function A for branch A")
 }
 
-func Feature_1(){
-	fmt.Println("This is the feature 1 for branch 1")
-}
--- main.go
+++ main.go
@@ -6,6 +6,3 @@ func main() {
 	fmt.Println("This is the main function")
 }
 
-func feature_2(){
-	fmt.Println("This is the feature 2 for branch 2")
-}
